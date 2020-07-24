0abeb151e8d72cbaad746b35a97894696b0dddeb
Boris Gromov <0xff.root@gmail.com>
2020-07-24 12:03:06 +0200

    Clean up

--- Package.swift
+++ Package.swift
@@ -1,14 +1,10 @@
 // swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "VolalyLocalization",
-    platforms: [
-        .iOS(.v13),
-        .macOS(.v10_13)
-    ],
+    platforms: [.iOS(.v13), .macOS(.v10_13)],
     products: [
         .library(
             name: "VolalyLocalization",
