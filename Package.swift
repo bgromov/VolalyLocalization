5847c81d2e0a3dbee7a87b72b6ae9a7abd6a7c3a
Boris Gromov <0xff.root@gmail.com>
2020-08-19 14:29:04 +0200

    Fix: use canonical git URL for dependency

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["CRelloc", "VolalyLocalization"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/bgromov/TransformSwift", from: "0.1.0"),
+        .package(url: "https://github.com/bgromov/TransformSwift.git", from: "0.1.0"),
     ],
     targets: [
         .target(
